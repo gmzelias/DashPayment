67156a7e59f82578710cc07cfb17051b17c3b83f
gmzelias <gmzelias@gmail.com>
2018-09-24 18:18:57 -0400

    2nd day

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const pjson = require('../test.json');
 const cars = require('../carsData.json');
+var mysql      = require('mysql');
+var moment = require('moment');
+var addrValidator = require('wallet-address-validator');
+
+var pool      =    mysql.createPool({
+  connectionLimit : 100, //important
+  host     : 'localhost',
+  user     : 'Elias',
+  password : '18003154dash',
+  database : 'dpayments',
+  debug    :  false
+});
 
 
+//Front Page
 router.get('/', function (req, res) {
   res.render('index', { 
     title: 'Home', 
@@ -11,20 +24,68 @@ router.get('/', function (req, res) {
     name: pjson.name 
 })
 })
-
+//Submit Button
 router.post('/submit', function (req, res, next) {
-  console.log('hello');
-  console.log(req.body);
-  /*response = {
-    id : req.body.first_name,
-    address : req.body.last_name,
-    ammount: req.body.gender
-    };
-  res.render('submit', { 
-    title: 'Home', 
-    message: pjson.formM, 
-    name: pjson.name 
-})*/
+    //Validate information
+    //-------------------------
+      var validated;
+      var data = {
+      InvoiceID : req.body.Invoice,
+      SAddress :'', 
+      RAddress : req.body.PubAddress, //attention
+      Ammount :req.body.Ammount, 
+      Date :moment().format('llll')
+  };
+    //Validate address information
+    //-------------------------------
+      var addrCheck = addrValidator.validate(data.RAddress, 'DASH');
+        if(addrCheck)
+          runQuery(setValue);
+       else
+       res.render('submit', {validated:false});
+
+   function runQuery(callback) {
+      pool.query('INSERT INTO paymentinfo SET ?', data, function (error, results, fields) {
+        if (!error){
+        console.log('Query executed.');
+        validated = true;
+        }
+        else{
+        console.log('Error while performing Query.');
+        validated = false;
+        }
+      callback();
+    });
+  };
+  function setValue() {
+    var data2 = {
+      id: req.body.Invoice, 
+      address: req.body.PubAddress, 
+      ammount: req.body.Ammount,
+      validated:validated
+      }; 
+    res.render('submit', data2);
+  };
+  
+
+    //Select Query
+   // --------------
+    /*pool.query('SELECT * from paymentinfo', function(err, rows, fields) {
+        if (!err)
+           console.log('The solution is: ', rows);
+        else
+        console.log('Error while performing Query.');
+    });*/
+
+    //Activate for POSTMAN use:
+    // ---------------------------
+    /*res.writeHead(200, {"Content-Type": "application/json"});
+    var json = JSON.stringify({ 
+      Invoice: req.body.Invoice, 
+      PubAddress: req.body.PubAddress, 
+      Ammount: req.body.Ammount
+    });
+    res.end(json);*/
 })
 
 router.get('/about', function (req, res) {
